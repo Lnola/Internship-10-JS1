59607e04c549a92ba725382c8f72722628642f1c
Lnola <nola.luka@gmail.com>
2019-03-07 23:47:10 +0100

    First task done

--- scrypt.js
+++ scrypt.js
@@ -1,22 +1,50 @@
-let userChoice = prompt("Odaberi redni broj algoritma koji zelis izvrsiti\n\n"
-    + " 1 \u25B8 Sentence reverse\n\n"
-    + " 2 \u25B8 Max letters\n\n"
-    + " 3 \u25B8 Capitalize\n\n"
-    + " 4 \u25B8 Word reverse\n\n"
-    + " KRAJ \u25B8 Kraj programa");
-let sentence = prompt("Unesite tekst za algoritam", "Unesi recenicu");
+let userChoice;
+let sentence = "";
+let selection=["1","2","3","4","1.","2.","3.","4."]
+do {
+    userChoice = prompt("Odaberi redni broj algoritma koji zelis izvrsiti\n\n"
+        + " 1 \u25B8 Sentence reverse\n\n"
+        + " 2 \u25B8 Max letters\n\n"
+        + " 3 \u25B8 Capitalize\n\n"
+        + " 4 \u25B8 Word reverse\n\n"
+        + " KRAJ \u25B8 Kraj programa");
 
-let reverseSentence = reverseString(sentence).join("");
-console.log(reverseSentence);
+    if (userChoice !== null && userChoice !== "KRAJ" && userChoice !== "kraj" && userChoice !== "Kraj" && userChoice !== "" && selection.includes(userChoice)) {
+        sentence = prompt("Unesite tekst za algoritam", "Unesi recenicu");
+        if (sentence === undefined || sentence === "" || sentence === null) {
+            alert("Moras unjeti recenicu")
+            console.log(sentence);
+        }
+    }
 
-let maxLetter = getMaxLetter(sentence);
-console.log(maxLetter);
+    switch (userChoice) {
+        case "1" || "1.":
+            let reverseSentence = reverseString(sentence).join("");
+            alert(reverseSentence);
+            break;
+        case "2" || "2.":
+            let maxLetter = getMaxLetter(sentence);
+            alert("Slovo " + maxLetter.letter + " pojavljuje se " + maxLetter.numberOfRepetitions + " puta");
+            break;
+        case "3" || "3.":
+            let capitaliseSentence = capitaliseWords(sentence);
+            alert(capitaliseSentence);
+            break;
+        case "4" || "4.":
+            let wordReverse = reverseOnlyWords(sentence);
+            alert(wordReverse);
+            break;
+        case "kraj" || "KRAJ" || "Kraj":
+            if (confirm("Zelis li prekinuti program?"))
+                userChoice = null;
+            break;
+        case null:
+            break;
+        default:
+            alert("Unesi redni broj algoritma za nastavak ili rijec Kraj za izlazak");
+    }
 
-let capitaliseSentence = capitaliseWords(sentence);
-console.log(capitaliseSentence);
-
-let wordReverse = reverseOnlyWords(sentence);
-console.log(wordReverse);
+} while (userChoice !== null)
 
 
 // FUNCTIONS
